427a6f0596029fefb1106c4e1ff54e3538023e41
Thais Silveira <thais_lsilveira@hotmail.com>
2019-09-19 21:22:13 -0300

    first commit

---
+++ src/repositories/customer-repository.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Customer = mongoose.model('Customer');
+
+exports.get = async() => {
+    const res = await Customer.find({
+        active: true
+    }, 'title price slug');
+    return res;
+}
+
+exports.create = async(data) => {
+    var customer = new Customer(data);
+    await customer.save();
+}
