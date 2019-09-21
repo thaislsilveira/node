b28449e82988b39929d9aa6469c0e00d58952b12
Thais Silveira <thais_lsilveira@hotmail.com>
2019-09-21 19:22:00 -0300

    Recuperando dados do usuário logado

--- src/controllers/customer-controller.js
+++ src/controllers/customer-controller.js
@@ -3,7 +3,7 @@
 const ValidationContract = require('../validators/fluent-validator');
 const repository = require('../repositories/customer-repository');
 const md5 = require('md5');
-
+const authService = require('../services/auth-service');
 const emailService = require('../services/email-service');
 
 exports.post = async(req, res, next) => {
@@ -28,6 +28,41 @@ exports.post = async(req, res, next) => {
     emailService.send(req.body.email, 'Bem vindo ao node Store', global.EMAIL_TMPL.replace('{0}', req.body.name));
     res.status(201).send({ 
         message: 'Cliente cadastrado com sucesso!'});
+   }catch(e){
+    res.status(500).send({
+        message: 'Falha ao processar sua requisição'
+    });
+   }
+};
+
+exports.authenticate = async(req, res, next) => {
+   try{
+   const customer = await repository.authenticate({
+        email: req.body.email,
+        password: md5(req.body.password + global.SALT_KEY)
+    });
+
+    if(!customer){
+        res.status(404).send({
+            message : 'Usuário ou senha inválidos'
+        });
+        return;
+    }
+
+    const token = await authService.generateToken({
+        id: customer._id,
+        email: customer.email, 
+        name: customer.name
+    });
+
+
+    res.status(201).send({
+        token: token,
+        data: {
+            email: customer.email, 
+            name: customer.name
+        }
+    });
    }catch(e){
     res.status(500).send({
         message: 'Falha ao processar sua requisição'
